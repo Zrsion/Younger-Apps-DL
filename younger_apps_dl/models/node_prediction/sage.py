b70d7a522398ef5b8e4754d5c01f5ae752d3069b
Zrsion <1079451538@qq.com>
2025-05-13 20:15:36 +0800

    update

--- younger_apps_dl/models/node_prediction/sage.py
+++ younger_apps_dl/models/node_prediction/sage.py
@@ -12,39 +12,6 @@
 # This source code is licensed under the Apache License 2.0 found in the
 # LICENSE file in the root directory of this source tree.
 ########################################################################
-
-import torch
-from torch import nn
-from torch.nn import Embedding
-from torch.nn import functional as F
-from torch_geometric.nn import SAGEConv
-
-
-# class SAGE_NP(nn.Module):
-
-#     def __init__(self, node_dict_size, node_dim, hidden_dim, dropout, layer_number=3):
-#         super(SAGE_NP, self).__init__()
-#         self.node_embedding_layer = Embedding(node_dict_size, node_dim)
-#         self.dropout = dropout
-#         self.layer_1 = SAGEConv(node_dim, hidden_dim)
-#         self.layer_2 = SAGEConv(hidden_dim, node_dict_size)
-#         self.initialize_parameters()
-
-#     def forward(self, x, edge_index):
-#         x = self.node_embedding_layer(x).squeeze(1)
-#         x = F.dropout(x, p=self.dropout, training=self.training)
-#         x = self.layer_1(x, edge_index)
-#         x = F.relu(x)
-#         x = F.dropout(x, p=self.dropout, training=self.training)
-#         x = self.layer_2(x, edge_index)
-#         return x 
-
-#     def initialize_parameters(self):
-#         nn.init.normal_(self.node_embedding_layer.weight, mean=0, std=self.node_embedding_layer.embedding_dim ** -0.5)
-
-
-import torch
-import numpy
 from torch import nn
 from torch.nn import Embedding
 from torch.nn import functional as F
